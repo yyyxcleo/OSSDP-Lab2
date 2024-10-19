049345d0aa786c3e40f5f2397b5866719fc986aa
LEO <2807090621@qq.com>
2024-10-19 17:17:53 +0800

    debug

--- Solution5.java
+++ Solution5.java
@@ -41,6 +41,54 @@ import java.util.Arrays;
  * 1 <= target <= 106
  *
  */
+// class Solution5 {
+//     static final int P = 1000000007;
+//     static final int MAX_N = 100005;
+
+//     int[] f = new int[MAX_N];
+
+//     public int numSubseq(int[] nums, int target) {
+//         pretreatment();
+
+//         Arrays.sort(nums);
+
+//         int ans = 0;
+//         for (int i = 0; i < nums.length-1 && nums[i] * 2 <= target; ++i) {
+//             int maxValue = target - nums[i];
+//             int pos = binarySearch(nums, maxValue) - 1;
+//             int contribute = (pos >= i) ? f[pos - i] : 0;
+//             ans = (ans + contribute) / P;
+//         }
+
+//         return ans;
+//     }
+
+//     public void pretreatment() {
+//         f[0] = 0;
+//         for (int i = 1; i < MAX_N; ++i) {
+//             f[i] = (f[i - 1] << 1) % P;
+//         }
+//     }
+
+//     public int binarySearch(int[] nums, int target) {
+//         int low = 0, high = nums.length;
+//         while (low <= high) {
+//             int mid = (high - low) / 2 + low;
+//             if (mid == nums.length) {
+//                 return mid;
+//             }
+//             int num = nums[mid];
+//             if (num <= target) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid;
+//             }
+//         }
+//         return low;
+//     }
+// }
+import java.util.Arrays;
+
 class Solution5 {
     static final int P = 1000000007;
     static final int MAX_N = 100005;
@@ -53,25 +101,26 @@ class Solution5 {
         Arrays.sort(nums);
 
         int ans = 0;
-        for (int i = 0; i < nums.length-1 && nums[i] * 2 <= target; ++i) {
+        int n = nums.length;  // 存储数组长度
+        for (int i = 0; i < n && nums[i] * 2 <= target; ++i) {
             int maxValue = target - nums[i];
             int pos = binarySearch(nums, maxValue) - 1;
             int contribute = (pos >= i) ? f[pos - i] : 0;
-            ans = (ans + contribute) / P;
+            ans = (ans + contribute) % P;  // 使用%而不是/
         }
 
         return ans;
     }
 
     public void pretreatment() {
-        f[0] = 0;
+        f[0] = 1;  // 修正：初始化f[0]为1
         for (int i = 1; i < MAX_N; ++i) {
-            f[i] = (f[i - 1] << 1) % P;
+            f[i] = (f[i - 1] << 1) % P;  // 左移操作的结果要取模
         }
     }
 
     public int binarySearch(int[] nums, int target) {
-        int low = 0, high = nums.length;
+        int low = 0, high = nums.length - 1;  // 修正：high为nums.length - 1
         while (low <= high) {
             int mid = (high - low) / 2 + low;
             if (mid == nums.length) {
@@ -81,9 +130,28 @@ class Solution5 {
             if (num <= target) {
                 low = mid + 1;
             } else {
-                high = mid;
+                high = mid - 1;  // 修正：high更新为mid - 1
             }
         }
         return low;
     }
+}
+
+public static void main(String[] args) {
+    Solution5 solution = new Solution5();
+    
+    // 示例1
+    int[] nums1 = {3, 5, 6, 7};
+    int target1 = 9;
+    System.out.println(solution.numSubseq(nums1, target1)); // 输出: 4
+    
+    // 示例2
+    int[] nums2 = {3, 3, 6, 8};
+    int target2 = 10;
+    System.out.println(solution.numSubseq(nums2, target2)); // 输出: 6
+
+    // 示例3
+    int[] nums3 = {2, 3, 3, 4, 6, 7};
+    int target3 = 12;
+    System.out.println(solution.numSubseq(nums3, target3)); // 输出: 61
 }
